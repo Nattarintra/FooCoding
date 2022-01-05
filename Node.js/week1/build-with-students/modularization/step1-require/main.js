f384d3880bf3ec9b04bad0b633986c3a051d472b
Nattarintra Thutsanti <nattarintra@headofsearch.se>
2022-01-05 11:16:38 +0100

    Remove Node.js

--- Node.js/week1/build-with-students/modularization/step1-require/main.js
+++
@@ -1,5 +0,0 @@
-// require is a special function that is available in node.js
-// it reads the module/package (in this case a file) and executes it
-const math = require("./math.js");
-
-console.log(math.add(math.two, 2));
